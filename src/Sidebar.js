cafd2ac835c6de03780f9e9a176c08ac8f2f4086
DanielIbiapina <daniel18.ibiapinabarros@gmail.com>
2024-10-23 02:25:38 -0300

    feat: fotos diversas

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,19 +2,17 @@ import Usuario from "./Usuario";
 import Sugestoes from "./Sugestoes";
 
 export default function Sidebar() {
-    return (
-        <div class="sidebar">
-            <Usuario />
-            <Sugestoes />
+  return (
+    <div class="sidebar">
+      <Usuario />
+      <Sugestoes />
 
+      <div class="links">
+        Sobre • Ajuda • Imprensa • API • Carreiras • Privacidade • Termos •
+        Localizações • Contas mais relevantes • Hashtags • Idioma
+      </div>
 
-            <div class="links">
-                Sobre • Ajuda • Imprensa • API • Carreiras • Privacidade • Termos • Localizações • Contas mais relevantes • Hashtags • Idioma
-            </div>
-
-            <div class="copyright">
-                © 2021 INSTAGRAM DO FACEBOOK
-            </div>
-        </div>
-    );
+      <div class="copyright">© 2021 INSTAGRAM DO FACEBOOK</div>
+    </div>
+  );
 }
